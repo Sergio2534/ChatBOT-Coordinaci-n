d9fc4ecfd4778c3f5c2d6c34d9eed2a3083727e4
Sergio Jimenez Magaña <sergio.jimenez5486@alumnos.udg.mx>
2025-05-15 22:09:19 -0600

    Cambios semana 6

--- cnn.py
+++ cnn.py
@@ -1,162 +1,140 @@
-import numpy as np
-import util
-import tensorflow as tf
-from tensorflow import keras
-
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Layer, Flatten, LeakyReLU
-from tensorflow.keras.layers import Input, Reshape, Dense, Lambda
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D
-
-from tensorflow.keras.layers import Conv1D, UpSampling1D
-from tensorflow.keras.layers import AveragePooling1D, MaxPooling1D
-
-from tensorflow.keras.layers import LSTM, Dropout
-
-from tensorflow.keras import backend as K
-
-from tensorflow.keras.optimizers import Adam, SGD, RMSprop
-from tensorflow.keras.losses import mse, binary_crossentropy
-from tensorflow.keras import regularizers, activations, initializers, constraints
-from tensorflow.keras.constraints import Constraint
-from tensorflow.keras.callbacks import History, EarlyStopping
-
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras.models import load_model
-
-from tensorflow.python.keras.utils.generic_utils import get_custom_objects
-
-import string
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from tqdm import tqdm
-
-import matplotlib.cm as cm
-from matplotlib.colors import Normalize
-
-def RMSE(x, y):
-    return np.sqrt(np.mean(np.square(x.flatten() - y.flatten())))
-    
-class CNN:
-
-    def __init__(self, data, n_lag, n_seq, name=[]):
-    
-        self.name = name
-        
-        self.data = data
-        self.n_lag = n_lag
-        self.n_seq = n_seq
-        
-        self.n_batch = 0
-        self.nb_epoch = 0
-        
-        self.x = []
-        self.y = []
-        
-        self.model = []
-        
-    def get_cnn_model(self, input, output):
-    
-        input_data = Input(shape=(input.shape[1], input.shape[2]))
-        
-        _ = Conv1D(16, 3, padding='same', data_format='channels_last')(input_data)
-        _ = LeakyReLU(alpha=0.3)(_)
-        _ = MaxPooling1D(2, padding="same")(_)
-        
-        _ = Conv1D(8, 6, padding='same', data_format='channels_last')(_)
-        _ = LeakyReLU(alpha=0.3)(_)
-        _ = MaxPooling1D(2, padding="same")(_)
-        
-        _ = Flatten()(_)
-        _ = Dropout(0.3)(_)
-        
-        output_data = Dense(output.shape[1])(_)
-        
-        self.model = Model(input_data, output_data)
-        self.model.compile(loss='mse', optimizer=Adam(lr=1e-3, decay=1e-3 / 200))
-        self.model.summary()
-        plot_model(self.model, to_file='cnn.png')
-        
-    def train(self, n_batch=1, nb_epoch=100, load=False):
-    
-        self.n_batch = n_batch
-        self.nb_epoch = nb_epoch
-        
-        x = self.data.iloc[:, 0:self.n_lag].values
-        self.x = x.reshape(x.shape[0], x.shape[1], 1)
-        self.y = self.data.iloc[:, self.n_lag:].values
-        
-        self.get_cnn_model(self.x, self.y)
-        
-        history = History()
-
-        losses = np.zeros([self.nb_epoch, 2])
-        
-        for i in tqdm(range(self.nb_epoch)):
-        
-            self.model.fit(self.x, self.y, 
-                        epochs=1, batch_size=self.n_batch, verbose=False,
-                        shuffle=True, callbacks=[history])
-
-            losses[i, :]  = np.asarray(list(history.history.values()))[:, i]
-            print ("%d [Loss: %f] [Val loss: %f]" % (i, losses[i, 0], losses[i, 1]))
-            
-            figs = util.plotLosses(losses, name="cnn_losses")
-
-    def forecasts(self, test_data):
-    
-        forecasts = list()
-        
-        for i in range(len(test_data)):
-        
-            x = test_data.iloc[i, 0:self.n_lag].values
-            x = x.reshape(1, len(x), 1)
-            y = test_data.iloc[i, self.n_lag:].values
-            
-            y_hat = self.model.predict(x, batch_size=self.n_batch)
-            forecasts.append([x for x in y_hat[0, :]])
-            
-        return forecasts
-        
-    def forecasts_multistep(self, test_data, n_steps=3):
-    
-        n_steps = n_steps
-        forecasts_multi = list()
-
-        x = test_data.iloc[0, 0:self.n_lag].values
-        x = x.reshape(1, len(x), 1)
-
-        for i in range(n_steps):
-
-            y_hat = self.model.predict(x, batch_size=self.n_batch)
-            forecasts_multi.append(y_hat[0, :])
-            x = y_hat.reshape(1, y_hat.shape[1], 1)
-            
-        return forecasts_multi
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+from numpy import mean
+from numpy import std
+from numpy import dstack
+from pandas import read_csv
+from matplotlib import pyplot
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.utils import to_categorical
+ 
+# load a single file as a numpy array
+def load_file(filepath):
+	dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+	return dataframe.values
+ 
+# load a list of files and return as a 3d numpy array
+def load_group(filenames, prefix=''):
+	loaded = list()
+	for name in filenames:
+		data = load_file(prefix + name)
+		loaded.append(data)
+	# stack group so that features are the 3rd dimension
+	loaded = dstack(loaded)
+	return loaded
+ 
+# load a dataset group, such as train or test
+def load_dataset_group(group, prefix=''):
+	filepath = prefix + group + '/Inertial Signals/'
+	# load all 9 files as a single array
+	filenames = list()
+	# total acceleration
+	filenames += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+	# body acceleration
+	filenames += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+	# body gyroscope
+	filenames += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+	# load input data
+	X = load_group(filenames, filepath)
+	# load class output
+	y = load_file(prefix + group + '/y_'+group+'.txt')
+	return X, y
+ 
+# load the dataset, returns train and test X and y elements
+def load_dataset(prefix=''):
+	# load all train
+	trainX, trainy = load_dataset_group('train', prefix + 'HARDataset/')
+	print(trainX.shape, trainy.shape)
+	# load all test
+	testX, testy = load_dataset_group('test', prefix + 'HARDataset/')
+	print(testX.shape, testy.shape)
+	# zero-offset class values
+	trainy = trainy - 1
+	testy = testy - 1
+	# one hot encode y
+	trainy = to_categorical(trainy)
+	testy = to_categorical(testy)
+	print(trainX.shape, trainy.shape, testX.shape, testy.shape)
+	return trainX, trainy, testX, testy
+ 
+# standardize data
+def scale_data(trainX, testX, standardize):
+	# remove overlap
+	cut = int(trainX.shape[1] / 2)
+	longX = trainX[:, -cut:, :]
+	# flatten windows
+	longX = longX.reshape((longX.shape[0] * longX.shape[1], longX.shape[2]))
+	# flatten train and test
+	flatTrainX = trainX.reshape((trainX.shape[0] * trainX.shape[1], trainX.shape[2]))
+	flatTestX = testX.reshape((testX.shape[0] * testX.shape[1], testX.shape[2]))
+	# standardize
+	if standardize:
+		s = StandardScaler()
+		# fit on training data
+		s.fit(longX)
+		# apply to training and test data
+		longX = s.transform(longX)
+		flatTrainX = s.transform(flatTrainX)
+		flatTestX = s.transform(flatTestX)
+	# reshape
+	flatTrainX = flatTrainX.reshape((trainX.shape))
+	flatTestX = flatTestX.reshape((testX.shape))
+	return flatTrainX, flatTestX
+ 
+# fit and evaluate a model
+def evaluate_model(trainX, trainy, testX, testy, param):
+	verbose, epochs, batch_size = 0, 10, 32
+	n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+	# scale data
+	trainX, testX = scale_data(trainX, testX, param)
+	model = Sequential()
+	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps,n_features)))
+	model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(MaxPooling1D(pool_size=2))
+	model.add(Flatten())
+	model.add(Dense(100, activation='relu'))
+	model.add(Dense(n_outputs, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	# fit network
+	model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
+	# evaluate model
+	_, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+	return accuracy
+ 
+# summarize scores
+def summarize_results(scores, params):
+	print(scores, params)
+	# summarize mean and standard deviation
+	for i in range(len(scores)):
+		m, s = mean(scores[i]), std(scores[i])
+		print('Param=%s: %.3f%% (+/-%.3f)' % (params[i], m, s))
+	# boxplot of scores
+	pyplot.boxplot(scores, labels=params)
+	pyplot.savefig('exp_cnn_standardize.png')
+ 
+# run an experiment
+def run_experiment(params, repeats=10):
+	# load data
+	trainX, trainy, testX, testy = load_dataset()
+	# test each parameter
+	all_scores = list()
+	for p in params:
+		# repeat experiment
+		scores = list()
+		for r in range(repeats):
+			score = evaluate_model(trainX, trainy, testX, testy, p)
+			score = score * 100.0
+			print('>p=%s #%d: %.3f' % (p, r+1, score))
+			scores.append(score)
+		all_scores.append(scores)
+	# summarize results
+	summarize_results(all_scores, params)
+ 
+# run the experiment
+n_params = [False, True]
+run_experiment(n_params)
--- dataloader.py
+++ dataloader.py
@@ -1,135 +1,76 @@
-import random
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import os
-import time
-import scipy.io as sio
-import math
-import cv2
-import pandas as pd
-import subprocess
-import matplotlib as m
-
-class DataLoader:
-
-    def __init__(self, verbose=False):
-
-        self.verbose = verbose
-        
-        self.x = []
-        self.y = []
-        
-        self.cumm = []
-        self.label = []
-        
-        self.n_lag = 3
-        self.n_seq = 3
-        
-        self.ref = []
-    
-    def load_data(self):
-    
-        df = pd.read_csv("Data_Simulated_Bakken/DATA_BAKKEN.csv")
-        df = df.to_numpy()
-
-        x = df[:, 0:6]
-        self.y = df[:, 6:6+60]
-
-        x_min = np.min(x, axis=0)
-        x_max = np.max(x, axis=0)
-        self.x = (x - x_min)/(x_max - x_min)
-    
-        self.cumm = (np.sum(self.y, axis=-1)).flatten()
-        
-        #derive label for performance
-        partitions = [2100, 3700]
-        self.label = np.zeros(self.cumm.shape, dtype=np.int16)
-        self.label = np.where(self.cumm > partitions[0], 1, self.label)
-        self.label = np.where(self.cumm > partitions[1], 2, self.label)
-        
-        p = 400
-        x_train_sim = self.x[0:p, :]
-        x_test_sim = self.x[p:, :] 
-
-        train1 = self.cumm[0:p,]
-        test1 = self.cumm[p:,] 
-
-        train2 = self.label[0:p,]
-        test2 = self.label[p:,] 
-
-        from tensorflow.keras.utils import to_categorical
-        train2_one_hot = to_categorical(train2)
-        test2_one_hot = to_categorical(test2)
-        label_one_hot = to_categorical(self.label)
-        
-        if self.verbose:
-            #visualize the histograms
-            plt.figure(figsize=[12, 3.5])
-            plt.subplot(1, 3, 1)
-            plt.hist(self.cumm, bins=50)
-            for p in partitions:
-                plt.axvline(x=p, c='r', lw=3)
-            plt.xlim([0, 11000])
-            plt.title("Data")
-
-            plt.subplot(1, 3, 2)
-            #visualize the histograms (CDF)
-            plt.hist(self.cumm, bins=50, cumulative=True, density=True)
-            for p in partitions:
-                plt.axvline(x=p, c='r', lw=3)
-            plt.xlim([0, 11000])
-            plt.title("Data")
-
-            plt.subplot(1, 3, 3)
-            plt.hist(self.label)
-            plt.title("Data")
-            
-        return x_train_sim, x_test_sim, train1, test1, train2, test2, train2_one_hot, test2_one_hot
-    
-    def series_to_supervised(self, data, n_lag=1, n_seq=1, dropnan=True):
-      
-        n_vars = 1 if type(data) is list else data.shape[1]
-        df = pd.DataFrame(data)
-        cols, names = list(), list()
-        
-        for i in range(n_lag, 0, -1):
-            cols.append(df.shift(i))
-            names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-        
-        for i in range(0, n_seq):
-            cols.append(df.shift(-i))
-            if i == 0:
-                names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-            else:
-                names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-        
-        agg = pd.concat(cols, axis=1)
-        agg.columns = names
-        
-        if dropnan:
-            agg.dropna(inplace=True)
-        return agg
-
-    def get_time_series(self, ref_case=0, n_lag=1, n_seq=1, split=0.8):
-    
-        self.n_lag, self.n_seq = n_lag, n_seq
-        
-        np.random.seed(99)
-        
-        ref = (self.y[ref_case, :] - np.min(self.y[ref_case, :])) / (np.max(self.y[ref_case, :]) - np.min(self.y[ref_case, :]))
-        
-        self.ref = ref + np.random.normal(loc=0.0, scale=0.03, size=ref.shape)
-        
-        data = self.series_to_supervised(self.ref.tolist(), self.n_lag, self.n_seq)
-        
-        split_idx = int(data.shape[0]*split)
-        y_train_sim, y_test_sim = data[0:split_idx], data[split_idx:]
-        
-        return y_train_sim, y_test_sim
-
-    
-
-    
-    
-    
+from numpy import dstack
+from pandas import read_csv
+from keras.utils import to_categorical
+from matplotlib import pyplot
+ 
+# load a single file as a numpy array
+def load_file(filepath):
+	dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+	return dataframe.values
+ 
+# load a list of files and return as a 3d numpy array
+def load_group(filenames, prefix=''):
+	loaded = list()
+	for name in filenames:
+		data = load_file(prefix + name)
+		loaded.append(data)
+	# stack group so that features are the 3rd dimension
+	loaded = dstack(loaded)
+	return loaded
+ 
+# load a dataset group, such as train or test
+def load_dataset_group(group, prefix=''):
+	filepath = prefix + group + '/Inertial Signals/'
+	# load all 9 files as a single array
+	filenames = list()
+	# total acceleration
+	filenames += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+	# body acceleration
+	filenames += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+	# body gyroscope
+	filenames += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+	# load input data
+	X = load_group(filenames, filepath)
+	# load class output
+	y = load_file(prefix + group + '/y_'+group+'.txt')
+	return X, y
+ 
+# load the dataset, returns train and test X and y elements
+def load_dataset(prefix=''):
+	# load all train
+	trainX, trainy = load_dataset_group('train', prefix + 'HARDataset/')
+	print(trainX.shape, trainy.shape)
+	# load all test
+	testX, testy = load_dataset_group('test', prefix + 'HARDataset/')
+	print(testX.shape, testy.shape)
+	# zero-offset class values
+	trainy = trainy - 1
+	testy = testy - 1
+	# one hot encode y
+	trainy = to_categorical(trainy)
+	testy = to_categorical(testy)
+	print(trainX.shape, trainy.shape, testX.shape, testy.shape)
+	return trainX, trainy, testX, testy
+ 
+# plot a histogram of each variable in the dataset
+def plot_variable_distributions(trainX):
+	# remove overlap
+	cut = int(trainX.shape[1] / 2)
+	longX = trainX[:, -cut:, :]
+	# flatten windows
+	longX = longX.reshape((longX.shape[0] * longX.shape[1], longX.shape[2]))
+	print(longX.shape)
+	pyplot.figure()
+	xaxis = None
+	for i in range(longX.shape[1]):
+		ax = pyplot.subplot(longX.shape[1], 1, i+1, sharex=xaxis)
+		ax.set_xlim(-1, 1)
+		if i == 0:
+			xaxis = ax
+		pyplot.hist(longX[:, i], bins=100)
+	pyplot.show()
+ 
+# load data
+trainX, trainy, testX, testy = load_dataset()
+# plot histograms
+plot_variable_distributions(trainX)
